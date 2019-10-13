e6057b39dc38623ca6e82de3febc2c14bf328169
Pavel Grudinskiy <mr.pv234@mail.ru>
2019-10-13 17:41:53 +0300

    completed task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let brackets = new Map(bracketsConfig);
+  let arr = [];
+  for (let index = 0; index < str.length; index++) {
+    let char = str[index];
+    if(char == brackets.get(arr[arr.length - 1]))
+      arr.pop();
+    else
+      arr.push(char);
+  }
+  return arr.length == 0;
 }
